66fda62da67344b89113d2d10e20bc87931cf42b
Matija <mlukanic123@gmail.com>
2025-02-13 20:43:11 +0100

    edit Book properties

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -5,29 +5,43 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Metadata\ApiProperty;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Put;
 
-
-#[ApiResource] // Expose this entity via API Platform
+#[ApiResource]
 #[ORM\Entity]
 #[ORM\Table(name: "books")]
 class Book
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue]
     #[ORM\Column(type: "string", length: 20, unique: true)]
     #[ApiProperty(identifier: true)]
-    private string $isbn;
+    #[Assert\NotBlank]  // Ensures isbn is not empty
+    #[Assert\Length(min: 10, max: 20)]  // ISBN should be between 10-20 characters
+    private ?string $isbn = null;
 
     #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 255)]  // Title must be between 2-255 characters
     private string $title;
 
     #[ORM\Column(type: "string", length: 255)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 255)]  // Author must be between 2-255 characters
     private string $author;
 
     #[ORM\Column(type: "integer")]
+    #[Assert\NotBlank]
+    #[Assert\LessThanOrEqual(2100)]  // Ensures the year is not in the far future
     private int $publishedYear;
 
     #[ORM\Column(type: "string", length: 100)]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 100)]  // Genre must be between 2-100 characters
     private string $genre;
 
     // Getters and Setters
